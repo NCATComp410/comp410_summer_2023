e86b1e179c23c58be406fdb193d2b739238f463a
claesmk <claesmk@gmail.com>
2023-05-27 17:30:20 -0500

    Added UUID generation for Sprint-1

--- pii_scan.py
+++ pii_scan.py
@@ -1,7 +1,16 @@
+import uuid
+
+
 def show_aggie_pride():
     """Show Aggie Pride"""
     return 'Aggie Pride - Worldwide'
 
 
+def generate_uuid():
+    """Generate a UUID"""
+    return uuid.uuid4()
+
+
 if __name__ == '__main__':
     print(show_aggie_pride())
+    print(generate_uuid())
