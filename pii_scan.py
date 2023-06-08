4df18c89636dc2e4e80c38a4ec483a13a566344d
claesmk <claesmk@gmail.com>
2023-06-07 20:08:19 -0400

    Initial recognizer for sprint2

--- pii_scan.py
+++ pii_scan.py
@@ -1,4 +1,15 @@
 import uuid
+import spacy
+from presidio_analyzer import AnalyzerEngine, RecognizerRegistry, PatternRecognizer, Pattern, RecognizerResult
+
+# make sure en_core_web_lg is loaded correctly
+try:
+    nlp = spacy.load("en_core_web_lg")
+except OSError:
+    from spacy.cli import download
+
+    download("en_core_web_lg")
+    nlp = spacy.load("en_core_web_lg")
 
 
 def show_aggie_pride():
@@ -9,6 +20,50 @@ def show_aggie_pride():
 def generate_uuid():
     """Generate a UUID"""
     return uuid.uuid4()
+
+
+def analyze_text(text: str, show_supported=False) -> list[RecognizerResult]:
+    # Overview of Presidio
+    # https://microsoft.github.io/presidio/analyzer/
+
+    # Supported entities and adding custom entities
+    # https://microsoft.github.io/presidio/supported_entities/
+    # https://microsoft.github.io/presidio/analyzer/adding_recognizers/
+
+    # Create an additional pattern to detect a 8-4-4-4-12 UUID
+    uuid_pattern = Pattern(name='uuid_pattern',
+                           regex=r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
+                           score=0.9)
+    uuid_recognizer = PatternRecognizer(supported_entity='UUID', patterns=[uuid_pattern])
+
+    # Make US-SSN a little more strict
+    ssn_pattern = Pattern(name='ssn_pattern', regex=r'\b\d{3}-\d{2}-\d{4}\b', score=0.9)
+    ssn_recognizer = PatternRecognizer(supported_entity='US_SSN', patterns=[ssn_pattern])
+
+    # Initialize the recognition registry
+    registry = RecognizerRegistry()
+    registry.load_predefined_recognizers()
+
+    # Add custom recognizers
+    registry.add_recognizer(uuid_recognizer)
+    registry.add_recognizer(ssn_recognizer)
+
+    # Set up analyzer with our updated recognizer registry
+    analyzer = AnalyzerEngine(registry=registry)
+
+    # Show all entities that can be detected for debugging
+    if show_supported:
+        print(analyzer.get_supported_entities())
+        return []
+
+    # List of entities to detect
+    detect_types = ['US_SSN', 'PHONE_NUMBER', 'EMAIL_ADDRESS', 'PERSON', 'CREDIT_CARD',
+                    'UUID']
+
+    results = analyzer.analyze(text=text,
+                               entities=detect_types,
+                               language='en')
+    return results
 
 
 if __name__ == '__main__':
--- test_pii_scan.py
+++ test_pii_scan.py
@@ -1,10 +1,31 @@
 import unittest
-from pii_scan import show_aggie_pride
+from pii_scan import show_aggie_pride, analyze_text
+from presidio_analyzer import RecognizerResult
 
 
 class TestPIIScan(unittest.TestCase):
     def test_aggie_pride(self):
         self.assertEqual('Aggie Pride - Worldwide', show_aggie_pride())
+
+    def test_show_supported_entities(self):
+        # test the show_supported_entities function
+        results = analyze_text('This is a test', show_supported=True)
+        self.assertTrue(True)
+
+    def test_uuid(self):
+        # test a valid UUID for detection
+        results = analyze_text('This is a UUID: 123e4567-e89b-12d3-a456-426652340000')
+        self.assertEqual([RecognizerResult(entity_type='UUID', start=16, end=52, score=0.9)],
+                         results)
+
+        # test some uppercase letters
+        results = analyze_text('This is a UUID: 123E4567-e89B-12d3-a456-4266523400FF')
+        self.assertEqual([RecognizerResult(entity_type='UUID', start=16, end=52, score=0.9)],
+                         results)
+
+        # test an invalid UUID for detection
+        results = analyze_text('This is not a UUID: 123e4567-e89b-12d3-a456-42665234000')
+        self.assertEqual([], results)
 
 
 if __name__ == '__main__':
