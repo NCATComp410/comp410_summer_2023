b8a0b22d79961e4b70598430d62a968216e9bd20
jrbonds <56128385+jrbonds@users.noreply.github.com>
2023-06-27 05:44:51 +0000

    Added name detection

--- test_pii_scan.py
+++ test_pii_scan.py
@@ -37,6 +37,24 @@ class TestPIIScan(unittest.TestCase):
 
         results = analyze_text('This is a democracy')
         self.assertNotIn('NRP', str(results))
+    
+    def test_name_detection(self):
+        # test a valid name
+        results = analyze_text('Jason Bond')
+        self.assertIn('PERSON', str(results))
+
+        results = analyze_text('Primrose R. Everdean')
+        self.assertIn('PERSON', str(results))
+
+        results = analyze_text('Tony Robbins is my favorite salesman.')
+        self.assertIn('PERSON', str(results))
+
+        results = analyze_text('Jada Pinkett-Smith')
+        self.assertIn('PERSON', str(results))
+
+        results = analyze_text('This is a regular sentence.')
+        self.assertNotIn('PERSON', str(results))
+
 
     def test_phone_number_detection(self):
         # test a valid phone number
@@ -91,9 +109,8 @@ class TestPIIScan(unittest.TestCase):
         results = analyze_text('(999)222-444444')
         self.assertNotIn('PHONE_NUMBER', str(results))
 
-        esults = analyze_text('thisisanemail@gmail.com')
+        results = analyze_text('thisisanemail@gmail.com')
         self.assertNotIn('PHONE_NUMBER', str(results))
-
 
 
 if __name__ == '__main__':
